221c67730fa440a4d329ec682244994239b34709
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-19 14:04:12 +0300

    Fetch NASA API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import SideBar from './components/SideBar';
 
 function App() {
 
-    const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
     const [showModal, setShowModal] = useState(false);
 
     function handleToggleModal() {
         setShowModal(state => !state);
     }
+
+    useEffect(() => {
+        (async () => {
+            const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
+
+            console.log(url)
+            try {
+
+                const res = await fetch(url);
+                const data = await res.json();
+
+                console.log(data)
+
+            } catch (err) {
+                console.log(err.message);
+
+            }
+
+        })();
+    }, []);
 
     return (
         <>
