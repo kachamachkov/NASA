820f658bb1df821284e2b9f69da192b2adbcd306
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-19 14:48:18 +0300

    Add caching

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import SideBar from './components/SideBar';
 function App() {
 
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
     const [showModal, setShowModal] = useState(false);
 
     function handleToggleModal() {
@@ -18,11 +17,25 @@ function App() {
             const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
             const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
 
+            const today = (new Date()).toDateString();
+            const localKey = `NASA-${today}`;
+
+            if (localStorage.getItem(localKey)) {
+                const apiData = JSON.parse(localStorage.getItem(localKey));
+                setData(apiData);
+                console.log('Fetched from cache today');
+                return;
+            }
+
+            localStorage.clear();
+
             try {
 
                 const res = await fetch(url);
                 const apiData = await res.json();
 
+                localStorage.setItem(localKey, JSON.stringify(apiData));
+                console.log('Fetched from API today');
                 setData(apiData);
 
 
@@ -36,7 +49,6 @@ function App() {
 
     return (
         <>
-
             {data ? (<Main data={data} />) :
                 (<div className='loading'>
                     <i class="fa-solid fa-gear"></i>
