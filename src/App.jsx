138375aef5b459591f5903529d99b60176183355
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-19 13:46:34 +0300

    Add API key to .env

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SideBar from './components/SideBar';
 
 function App() {
 
+    const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
     const [showModal, setShowModal] = useState(false);
 
     function handleToggleModal() {
